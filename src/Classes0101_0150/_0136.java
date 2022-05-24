80ecf79f2db1f904f1c34ecc1ad03445132ee00a
J34993R <guialves.oliveira1234@gmail.com>
2022-05-24 15:04:02 -0300

    added exercise 136 single-number

--- src/Classes0101_0150/_0136.java
+++ src/Classes0101_0150/_0136.java
@@ -2,10 +2,19 @@ package src.Classes0101_0150;
 
 public class _0136 {
 
-    /*
-    Didn't tried an attempt
-     */
-
+/* Date: 24/05/2022
+ * User: Guilherme Alves de Oliveira
+ *
+ * Source: https://leetcode.com/problems/single-number/
+ *
+ * Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+ * You must implement a solution with a linear runtime complexity and use only constant extra space.
+ *
+ * Success
+ * Runtime: 1 ms, faster than 98.07% of Java online submissions for Single Number.
+ * Memory Usage: 49.9 MB, less than 69.24% of Java online submissions for Single Number.
+ *
+ */
 
     public int singleNumber(int[] nums) {
         int num=0;
